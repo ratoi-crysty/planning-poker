76d99bd83928318fe52e4ecd8639bc84b7a792eb
Cristian Ratoi <cristian.ratoi@everymatrix.com>
2021-05-09 12:27:06 +0300

    add heroku support

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { RoomModule } from './room/room.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthModule, RoomModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api*']
+    }),
+    AuthModule,
+    RoomModule,
+  ],
 })
 export class AppModule {
 }
